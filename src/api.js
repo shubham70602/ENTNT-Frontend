1da34e1be188caed0295ddbe168f3594beaf21e3
shubham <shubhamkannojiya45@gmail.com>
2025-01-02 15:55:35 +0530

    changes

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://entnt-backend-dsaq.onrender.com/api";
+const BASE_URL = "https://entnt-backend-2inq.onrender.com/api";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
